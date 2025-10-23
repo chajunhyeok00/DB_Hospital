b2f930ecb213ac472664ec7f27e9591ef0a38ba6
chajunhyeok00 <147304447+chajunhyeok00@users.noreply.github.com>
2025-10-24 05:34:35 +0900

    Add files via upload

---
+++ DB 테이블/차준혁.sql
@@ -0,0 +1,85 @@
+
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(1, 'Eunsoo Oh', 'Female', '761011-2641892', 'AB', '010-5387-0364', 140.9, 50.0);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(2, 'Jihae Nam', 'Female', '051130-3641892', 'B', '010-3778-4821', 175.0, 50.0);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(3, 'Minjun Kim', 'Male', '920312-1234567', 'O', '010-9876-1234', 180.2, 72.5);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(4, 'Seoyeon Lee', 'Female', '840715-2345678', 'A', '010-1234-5678', 158.4, 55.2);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(5, 'Jihun Park', 'Male', '950901-1122334', 'B', '010-3456-7890', 170.0, 68.0);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(6, 'Sujin Jeong', 'Female', '990102-3344556', 'AB', '010-8765-4321', 165.5, 60.3);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(7, 'Junhyuk Choi', 'Male', '810815-5566778', 'O', '010-6543-2109', 175.8, 78.4);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(8, 'Yujin Lee', 'Female', '021223-1122334', 'A', '010-7410-8523', 160.1, 54.7);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(9, 'Jimin Han', 'Female', '880101-4433221', 'AB', '010-8523-7410', 165.9, 59.0);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(10, 'Jeonghoon Bae', 'Male', '950512-6677889', 'B', '010-9632-1470', 172.2, 70.1);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(11, 'Seoyeong Kim', 'Female', '030102-3344556', 'A', '010-2580-3690', 160.5, 52.3);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(12, 'Hayoung Choi', 'Male', '010901-6677889', 'O', '010-1234-9852', 180.7, 82.4);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(13, 'Jihoo Yun', 'Male', '000101-1122334', 'B', '010-7777-8888', 174.9, 69.5);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(14, 'Minji Jang', 'Female', '960505-7788990', 'AB', '010-4567-7890', 155.6, 48.2);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(15, 'Taeyun Kang', 'Male', '750714-5566778', 'O', '010-8765-4321', 185.0, 85.6);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(16, 'Dahi Park', 'Female', '910123-1122334', 'A', '010-2580-3690', 162.3, 56.9);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(17, 'Minho Jeong', 'Male', '970212-3344556', 'B', '010-1234-5678', 170.4, 65.3);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(18, 'Seoyeong Oh', 'Female', '900909-7788990', 'AB', '010-3690-2580', 159.5, 53.8);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(19, 'Seungwoo Lee', 'Male', '950813-1122334', 'O', '010-8523-7410', 182.2, 78.0);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(20, 'Nayeong Yoo', 'Female', '921201-3344556', 'A', '010-1470-9632', 161.9, 57.5);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(21, 'Doyoon Kim', 'Male', '860701-5566778', 'B', '010-9876-1234', 178.8, 74.3);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(22, 'Jisoo Hong', 'Female', '820812-6677889', 'AB', '010-4567-7890', 154.4, 47.5);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(23, 'Jihyun Jeon', 'Female', '740610-4433221', 'O', '010-9632-1470', 168.9, 63.0);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(24, 'Minseong Kang', 'Male', '890515-7788990', 'A', '010-7410-8523', 176.7, 72.8);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(25, 'Jiyeong Yun', 'Female', '920623-6677889', 'AB', '010-3690-2580', 163.5, 58.7);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(26, 'Haneul Im', 'Male', '931203-1122334', 'O', '010-2580-3690', 181.0, 75.6);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(27, 'Yerin Kim', 'Female', '990401-7788990', 'A', '010-1234-5678', 159.7, 54.1);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(28, 'Taejun Lee', 'Male', '870919-6677889', 'B', '010-4567-7890', 171.3, 66.9);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(29, 'Sua Jeong', 'Female', '850208-3344556', 'AB', '010-8523-7410', 165.8, 60.4);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(30, 'Eunji Jang', 'Female', '820303-7788990', 'O', '010-1470-9632', 160.3, 53.6);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(31, 'Jiwon Cho', 'Female', '890101-5566778', 'A', '010-7777-2222', 163.0, 57.2);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(32, 'Seungmin Park', 'Male', '970809-1234567', 'B', '010-2580-3579', 177.3, 71.5);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(33, 'Hyojin Kim', 'Female', '990604-2345678', 'AB', '010-9999-1234', 164.8, 55.1);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(34, 'Taeyang Lee', 'Male', '920709-1234567', 'O', '010-3333-7777', 180.5, 80.2);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(35, 'Nari Park', 'Female', '960412-2345678', 'A', '010-4444-8888', 158.3, 50.0);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(36, 'Minseok Kim', 'Male', '860301-5566778', 'B', '010-2580-3690', 175.1, 72.9);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(37, 'Eunjin Jeong', 'Female', '900105-1234567', 'AB', '010-1234-5678', 160.6, 53.4);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(38, 'Jiwon Kim', 'Female', '021125-6677889', 'O', '010-8523-7410', 169.4, 62.3);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(39, 'Jihoon Bae', 'Male', '980904-7788990', 'B', '010-9999-3333', 178.2, 74.0);
+INSERT INTO Patient (PatientID, PatientName, Gender, NationalID, BloodType, PhoneNumber, Height, Weight) VALUES
+(40, 'Eunbi Choi', 'Female', '980307-1122334', 'A', '010-2580-9632', 162.7, 58.9);
+
+select * from patient;
+
+commit;
